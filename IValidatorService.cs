fe25746637d6222b1acc0ba8dd8dec724f34c32e
RimasBerz <116100732+RimasBerz@users.noreply.github.com>
2023-05-28 00:12:16 +0300

    Add files via upload

---
+++ IValidatorService.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Servicies
+{
+    public interface IValidatorService
+    {
+        bool ValidateNonEmpty(string input);
+        bool ValidateLogin(string login);
+    }
+}
---
+++ ValidatorService.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace WebApplication1.Servicies
+{
+    public class ValidatorService : IValidatorService
+    {
+        public bool ValidateNonEmpty(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool ValidateLogin(string login)
+        {
+            return Regex.IsMatch(login, @"^[a-zA-Z_][a-zA-Z0-9_]*$");
+        }
+    }
+}
